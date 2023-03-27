6ecc5a0bce026f55082fcfcfd0ca50d9ae91ac75
GuilhemCarriou <guilhem.carriou1@gmail.com>
2023-03-27 22:55:21 +0200

    Correct_fernet_and_time_

--- time_fernet_gui.py
+++ time_fernet_gui.py
@@ -14,32 +14,53 @@ DEFAULT_VALUES = {
     "pwd"  : "pwd_by_def" # Ajout d'un champ password 
 }
 TIME_TO_LIVE = 30 # durée de validité message (secondes)
+import os
+import base64
+import hashlib
+import logging
+import dearpygui.dearpygui as dpg
+from cryptography.fernet import Fernet
 
+from chat_client import ChatClient
+from cyphered_gui import CypheredGUI
+from generic_callback import GenericCallback
+DEFAULT_VALUES = {
+    "host" : "127.0.0.1",
+    "port" : "6666",
+    "name" : "foo",
+    "pwd"  : "pwd_by_def" # Ajout d'un champ password 
+}
+SIZE_KEY=16
 
 class TimeFernetGUI(FernetGUI):
     
     def __init__(self) -> None:
         super().__init__()
-        self._key=bytes()
-        #self._pwd=None
+
 
     def encrypt(self,_in:str)->tuple([bytes,bytes]):
+
+        iv=bytes(os.urandom(SIZE_KEY))
+        
         now_time=int(time.time())
         fernet=Fernet(self._key)
         encrypted=fernet.encrypt_at_time(bytes(_in,'utf-8'),current_time=now_time)
-        return (encrypted)
+
+        return (iv, encrypted)
         
     def decrypt(self,iv:bytes, encrypted:bytes)->str:
+
         iv=base64.b64decode(iv)
         encrypted=base64.b64decode(encrypted)
         now_time=int(time.time())
         fernet=Fernet(self._key)
+
         try:
-            iv_decrypted=fernet.decrypt_at_time(bytes(iv,'utf-8'),now_time,ttl=TIME_TO_LIVE)
-            message_decrypted=fernet.decrypt_at_time(bytes(encrypted,'utf-8'),now_time,ttl=TIME_TO_LIVE)
-            
-            result=iv_decrypted.decode('utf-8'),message_decrypted.decode('utf-8')
+            message_decrypted=fernet.decrypt_at_time(bytes(encrypted),current_time=now_time,ttl=TIME_TO_LIVE)
+            result=message_decrypted.decode('utf-8')
+
             return result
+        
         except InvalidToken as err:
             logging.error("Invalid Token")
             raise InvalidToken
